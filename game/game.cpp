c0c8513530a1d7bc7c3f7d1de0bea1d2318f0e6e
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-19 16:36:44 -0500

    Update game.cpp

--- game/game.cpp
+++ game/game.cpp
@@ -6,8 +6,18 @@
 //========================================================
 #include <iostream>
 using namespace std;
-#include <SFML/Graphics.hpp>
-using namespace sf; 
+//#include <SFML/Graphics.hpp>
+using namespace sf;
+#include "GlobalConstants.h"
+#include <time.h>
+#include "Navel.h"
+#include "Alien.h"
+#include "ArmyAlien.h"
+#include "Bullet.h"
+#include "bulletClump.h"
+#include "Bomb.h"
+#include "BombClump.h"
+#include "UI.h"
 
 //============================================================
 // YOUR HEADER WITH YOUR NAME GOES HERE. PLEASE DO NOT FORGET THIS
@@ -18,117 +28,186 @@ using namespace sf;
 // the current position of the ship. 
 // x is horizontal, y is vertical. 
 // 0,0 is in the UPPER LEFT of the screen, y increases DOWN the screen
-void moveShip(Sprite& ship)
-{
-	const float DISTANCE = 5.0;
 
-	if (Keyboard::isKeyPressed(Keyboard::Left))
-	{
-		// left arrow is pressed: move our ship left 5 pixels
-		// 2nd parm is y direction. We don't want to move up/down, so it's zero.
-		ship.move(-DISTANCE, 0);
-	}
-	else if (Keyboard::isKeyPressed(Keyboard::Right))
-	{
-		// right arrow is pressed: move our ship right 5 pixels
-		ship.move(DISTANCE, 0);
-	}
-}
-
+GameStateEnum resetState(int lives, int &destroyedAliens, GameStateEnum currentState, ArmyAlien &alienMafia, BulletClump &manyBullets, BombClump &manyBombs);
 
 
 int main()
 {
-	const int WINDOW_WIDTH = 800;
-	const int WINDOW_HEIGHT = 600;
+	srand(time(NULL)); //for random numbers later in loop
+	int randNum;
 
-	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "aliens!");
-	// Limit the framerate to 60 frames per second
-	window.setFramerateLimit(60);
+	int lives = 5; //lives of human
+	int destroyedAliens = 0; //number of aliens destroyed
 
-	// load textures from file into memory. This doesn't display anything yet.
-	// Notice we do this *before* going into animation loop.
-	Texture shipTexture;
-	if (!shipTexture.loadFromFile("ship.png"))
-	{
-		cout << "Unable to load ship texture!" << endl;
-		exit(EXIT_FAILURE);
-	}
-	Texture starsTexture;
-	if (!starsTexture.loadFromFile("stars.jpg"))
-	{
-		cout << "Unable to load stars texture!" << endl;
-		exit(EXIT_FAILURE);
-	}
+	GameStateEnum currentState = BIENVENIDO; //this is from the "GlobalConstants.h", options include: WELCOME, LEVEL_ONE, LEVEL_TWO, ALIEN_WON, HUMAN_WON
 
-	// A sprite is a thing we can draw and manipulate on the screen.
-	// We have to give it a "texture" to specify what it looks like
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "aliens!"); //where I get my wonderful title from
+	window.setFramerateLimit(60); //Limit the framerate to 60 frames per second
 
-	Sprite background;
-	background.setTexture(starsTexture);
-	// The texture file is 640x480, so scale it up a little to cover 800x600 window
-	background.setScale(1.5, 1.5);
-
-	// create sprite and texture it
-	Sprite ship;
-	ship.setTexture(shipTexture);
-
-
-	// initial position of the ship will be approx middle of screen
-	float shipX = window.getSize().x / 2.0f;
-	float shipY = window.getSize().y / 2.0f;
-	ship.setPosition(shipX, shipY);
-
+	//declare all instances of classes
+	UI userInterface(window);
+	Navel destroyer(window);
+	ArmyAlien alienMafia;
+	BulletClump manyBullets;
+	BombClump manyBombs;
 
 	while (window.isOpen())
 	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		// For now, we just need this so we can click on the window and close it
 		Event event;
-
 		while (window.pollEvent(event))
 		{
-			// "close requested" event: we close the window
-			if (event.type == Event::Closed)
+			if (event.type == Event::Closed) // "close requested" event: we close the window
 				window.close();
 			else if (event.type == Event::KeyPressed)
 			{
-				if (event.key.code == Keyboard::Space)
+				if (event.key.code == Keyboard::Space) //handle space bar
 				{
-					// handle space bar
+					manyBullets.newBullet(destroyer.getPosition()); //add a new bullet where the ship is located
 				}
-				
+			}
+			else if (event.type == Event::MouseButtonReleased) //handle mouse button pressed
+			{
+				Vector2f mousePos = window.mapPixelToCoords(Mouse::getPosition(window));
+				if (userInterface.detectStart(mousePos)) //if they pressed one of the buttons to start the game
+				{
+					currentState = LEVEL_ONE;
+
+					alienMafia.populateAlienList(currentState); //populate the alienArmy after pressing Start
+				}
 			}
 		}
 
 		//===========================================================
-		// Everything from here to the end of the loop is where you put your
+		// Everything from here to the end of the loop is where the
 		// code to produce ONE frame of the animation. The next iteration of the loop will
 		// render the next frame, and so on. All this happens ~ 60 times/second.
 		//===========================================================
 
-		// draw background first, so everything that's drawn later 
-		// will appear on top of background
-		window.draw(background);
+		if (currentState == WELCOME) //if the enum state is welcome
+		{
+			cout << "in welcome screen " << endl; ///TESTING CODE
+			userInterface.drawStartScreen(window); //draw the screen that belongs to welcome
+		}
+		else if (currentState == LEVEL_ONE || currentState == LEVEL_TWO) //if the enum state is level one or two
+		{
+			if (currentState == LEVEL_ONE) ///TESTING CODE
+			{
+				cout << "in level one " << endl;
+			}
+			else if (currentState == LEVEL_TWO) ///TESTING CODE
+			{
+				cout << "in level two " << endl;
+			}
 
-		moveShip(ship);
+			userInterface.drawGameScreen(window, lives, destroyedAliens);//draw game labels
 
-		// draw the ship on top of background 
-		// (the ship from previous frame was erased when we drew background)
-		window.draw(ship);
+			if (destroyer.isNavelHitByAnyBombs(manyBombs.getBombList(), lives)) //if the ship is hit by any bombs
+			{
+				alienMafia.resetAliensPos(); //bring aliens to top
+				destroyer.resetShipPos(window); //bring navel to center
+				manyBullets.clearBullets(); //clear allbullets on screen 
+				manyBombs.clearBombs(); //clear all bombs on screen
+			}
+			if (alienMafia.haveAliensReachedShip(lives)) //automatically resets the ship if reached by aliens
+			{
+				destroyer.resetShipPos(window);
+				manyBullets.clearBullets(); //clear all missiles on screen 
+				manyBombs.clearBombs(); //clear all bombs on screen
+			}
+			destroyer.moveNavel(window); //ship movement
 
+			alienMafia.detectHitAliens(manyBullets.getBulletList(), destroyedAliens); //see who got hit
+			alienMafia.moveAlienArmy(); //move aliens down
 
-		// end the current frame; this makes everything that we have 
-		// already "drawn" actually show up on the screen
-		window.display();
+			manyBullets.moveBulletClump(); //move missiles up
 
-		// At this point the frame we have built is now visible on screen.
-		// Now control will go back to the top of the animation loop
-		// to build the next frame. Since we begin by drawing the
-		// background, each frame is rebuilt from scratch.
+			//add code here that makes the bombs faster for level 2
+			if (currentState == LEVEL_ONE)
+			{
+				randNum = rand() % 60 - 1; //random new bombs (kind of slow)
 
-	} // end body of animation loop
+			}
+			else if (currentState == LEVEL_TWO)
+			{
+				randNum = rand() % 30 - 1; //random new bombs (kind of fast)
+			}
+			if (randNum == 3)
+			{
+				if (!alienMafia.isListEmpty())//make sure that the list of aliens has aliens in it before you make a bomb
+				{
+					Vector2f tempPos = alienMafia.getRandomAlienPos(window, destroyedAliens);
+					manyBombs.newBomb(tempPos);
+				}
+
+			}
+			manyBombs.moveBombClump(); //move bombs down
+
+			//make everything appear
+			destroyer.drawShip(window);
+			alienMafia.drawAlienArmy(window);
+			manyBullets.drawBulletClump(window);
+			manyBombs.drawBombGroup(window);
+
+			//chang the state of the game (if applicable)
+			currentState = resetState(lives, destroyedAliens, currentState, alienMafia, manyBullets, manyBombs);
+		}
+		else if (currentState == ALIEN_WON) //if the aliens have taken all of the navals's lives
+		{
+			cout << "An alien won loop " << endl; ///TESTING CODE
+			userInterface.drawWinnerScreen(window, "Alien"); //draw winner screen, saying the alien won
+
+			alienMafia.clearAliens();//clear out list
+			//reset the scores
+			destroyedAliens = 0;
+			lives = 5;
+
+		}
+		else if (currentState == NAVEL_WON) //if the human has taken all of the aliens' lives
+		{
+			cout << "in human won loop " << endl; ///TESTING CODE
+			userInterface.drawWinnerScreen(window, "Human"); //draw winner screen, saying the human won
+
+			alienMafia.clearAliens();//clear out list
+			//reset the scores
+			destroyedAliens = 0;
+			lives = 5;
+		}
+
+		window.display(); //display everything that was drawn
+	}
 
 	return 0;
 }
+
+
+//look for winners
+//reset current state of the enums if needed
+GameStateEnum resetState(int lives, int &destroyedAliens, GameStateEnum currentState, ArmyAlien &alienMafia, BulletClump &manyMissiles, BombClump &manyBombs)
+{
+	if (lives == 0)
+	{
+		currentState = ALIEN_WON;
+	}
+	else if (destroyedAliens == 10)
+	{
+		if (currentState == LEVEL_TWO)
+		{
+			currentState = NAVEL_WON; //this line must not be working
+		}
+		else
+		{
+			currentState = LEVEL_TWO;
+			//make changes for transitioning from level one to level two
+			alienMafia.clearAliens();//clear out list
+			manyMissiles.clearBullets(); //clear all missiles on screen 
+			manyBombs.clearBombs(); //clear all bombs on screen
+			alienMafia.populateAlienList(currentState);
+			destroyedAliens = 0; //back to having hit no aliens
+		}
+	}
+	return currentState;
+}
+
+
 
